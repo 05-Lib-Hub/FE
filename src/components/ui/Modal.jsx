c17a3aaed9814cc09b071f275a88da9e17e6f914
김준하 <junhakjh@gmail.com>
2023-12-01 14:01:21 +0900

    fix: modal 띄울 때 scroll 방지

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Modal({ children }) {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden';
+
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, []);
+
   return (
-    <div className="absolute top-0 left-0 w-full h-screen bg-black/30 z-50">
+    <div className="fixed top-0 left-0 w-full h-full bg-black/30 z-50">
       {children}
     </div>
   );
